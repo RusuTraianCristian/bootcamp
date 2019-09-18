0c5d36df49535b4590875345ba24338bf713a836
Rusu Traian Cristian <rusutraiancristian@gmail.com>
2019-09-18 12:12:56 +0300

    bootcamp

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React to create Bootcamp
         </a>
       </header>
     </div>
