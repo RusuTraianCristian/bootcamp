222cfe79aa8c52b391b012d01119a9c0112fc719
Rusu Traian Cristian <rusutraiancristian@gmail.com>
2019-10-13 22:42:20 +0300

    message

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Learn React4a4a44dsadssadsadsadsadsadsadsaddsaddsaddasdsa
+                  Learn React4a4a44dsadssadsadsadsadsadsadsaddsaddsadssssss
                 </a>
             </header>
         </div>
