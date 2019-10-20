b101e6d59de3c3222b58d9d8ec3dc8b92575b30b
Rusu Traian Cristian <RusuTraianCristian@users.noreply.github.com>
2019-10-20 22:18:05 +0300

    latest

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => { // comment this right here..
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Learn Reactdsads
+                  Learn React
                 </a>
             </header>
         </div>
